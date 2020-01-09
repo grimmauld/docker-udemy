40d29d87ce78f3b91296830d26b489469439ddd4
Richard Lee <rplee93@gmail.com>
2020-01-09 17:23:53 -0600

    compare config files

--- fib-calculator/server/index.js
+++ fib-calculator/server/index.js
@@ -33,13 +33,15 @@ const redisClient = redis.createClient({
 });
 const redisPublisher = redisClient.duplicate();
 
-// Express Route Handlers
+// Express route handlers
+
 app.get('/', (req, res) => {
   res.send('Hi');
 });
 
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
+
   res.send(values.rows);
 });
 
@@ -51,12 +53,15 @@ app.get('/values/current', async (req, res) => {
 
 app.post('/values', async (req, res) => {
   const index = req.body.index;
+
   if (parseInt(index) > 40) {
     return res.status(422).send('Index too high');
   }
+
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
   res.send({ working: true });
 });
 
