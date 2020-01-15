244156bd4fc1cdfb724fcf43c88f37eca73457bc
Richard Lee <rplee93@gmail.com>
2020-01-14 22:11:20 -0600

    prepared for deployment

--- fib-calculator/deploy.sh
+++ fib-calculator/deploy.sh
@@ -1,8 +1,16 @@
-docker build -t grimmauld/multi-client -f ./client/Dockerfile ./client
-docker build -t grimmauld/multi-server -f ./server/Dockerfile ./server
-docker build -t grimmauld/multi-worker -f ./worker/Dockerfile ./worker
-docker push grimmauld/multi-client
-docker push grimmauld/multi-server
-docker push grimmauld/multi-worker
+docker build -t grimmauld/multi-client:latest -t grimmauld/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t grimmauld/multi-server:latest -t grimmauld/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t grimmauld/multi-worker:latest -t grimmauld/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push grimmauld/multi-client:latest
+docker push grimmauld/multi-server:latest
+docker push grimmauld/multi-worker:latest
+docker push grimmauld/multi-client:$SHA
+docker push grimmauld/multi-server:$SHA
+docker push grimmauld/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=grimmauld/multi-server
+
+kubectl set image deployments/client-deployment client=grimmauld/multi-client:$SHA
+kubectl set image deployments/server-deployment server=grimmauld/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=grimmauld/multi-worker:$SHA
