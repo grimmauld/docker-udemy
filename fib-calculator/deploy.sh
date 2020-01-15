9448d5175aef2bc11009e276aa568b949a0f98c2
Richard Lee <rplee93@gmail.com>
2020-01-14 21:35:59 -0600

    update deploy.sh, add env config to travis

--- fib-calculator/deploy.sh
+++ fib-calculator/deploy.sh
@@ -1,3 +1,8 @@
 docker build -t grimmauld/multi-client -f ./client/Dockerfile ./client
 docker build -t grimmauld/multi-server -f ./server/Dockerfile ./server
 docker build -t grimmauld/multi-worker -f ./worker/Dockerfile ./worker
+docker push grimmauld/multi-client
+docker push grimmauld/multi-server
+docker push grimmauld/multi-worker
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=grimmauld/multi-server
